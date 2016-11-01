24cc9e8a6d0d72054eccc169554b0d61c91c0b88
Ryan Westlake <ryancwestlake@gmail.com>
2016-10-31 22:08:44 -0600

    Add buttons to Dashboard
    I added the buttons, they are not rerouting yet

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -4,6 +4,7 @@ import {
   Text,
   TextInput,
   TouchableHighlight,
+  Image,
   StyleSheet,
   NavigatorIOS,
   ActivityIndicator
@@ -25,11 +26,58 @@ const styles = StyleSheet.create({
 });
 
 export default class Dashboard extends Component {
+  makeBackground(btn) {
+    let obj = {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignSelf: 'stretch',
+      flex: 1
+    }
+
+    if(btn === 0) {
+      obj.backgroundColor = '#48BBEC';
+    } else if (btn === 1) {
+      obj.backgroundColor = '#E77AAE';
+    } else {
+      obj.backgroundColor = '#758BF4';
+    }
+    return obj;
+  }
+
+  goToProfile() {
+    console.log('profile func');
+  }
+
+  goToRepos() {
+    console.log('repos func');
+  }
+
+  goToNotes() {
+    console.log('notes func');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text> testing Dashboard comp</Text>
-        <Text> {JSON.stringify(this.props.userInfo)} </Text>
+        <Image source={{ uri: this.props.userInfo.avatar_url }} style={styles.image} />
+        <TouchableHighlight
+          style={this.makeBackground(0)}
+          onPress={() => this.goToProfile()}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View Profile </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(1)}
+          onPress={() => this.goToRepos()}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View Repos </Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={this.makeBackground(2)}
+          onPress={() => this.goToNotes()}
+          underlayColor='#88D4F5'>
+            <Text style={styles.buttonText}> View Notes </Text>
+        </TouchableHighlight>
       </View>
     );
   }
