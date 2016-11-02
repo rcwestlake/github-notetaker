1c6e85180522d247ec1276cf0289dae54e83af63
Ryan Westlake <ryancwestlake@gmail.com>
2016-11-02 07:02:02 -0600

    Add firebase api calls
    This is to store data

--- app/Utils/api.js
+++ app/Utils/api.js
@@ -8,5 +8,20 @@ export default api = {
   getRepos(username) {
     let url = `http://api.github.com/users/${username}/repos`;
     return fetch(url).then((res) => res.json());
+  },
+
+  getNotes(username) {
+    username = username.toLowerCase().trim();
+    let url = `https://githubnotetaker-b3939.firebaseio.com/${username}.json`;
+    return fetch(url).then((res) => res.json());
+  },
+
+  addNote(username, note) {
+    username = username.toLowerCase().trim();
+    let url = `https://githubnotetaker-b3939.firebaseio.com/${username}.json`
+    return fetch(username, {
+      method: 'post',
+      body: JSON.stringify(note)
+    }).then((res) => res.json());
   }
 }
